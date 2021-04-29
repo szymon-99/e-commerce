2e010f6ff5031f780e811b809bf7dc9ded08ba3b
Szymon Prusak <szampons.szymon@gmail.com>
2021-04-29 21:35:44 +0200

    router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Navbar, Sidebar, Footer } from './components'
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Navbar, Sidebar, Footer } from './components';
+import {
+  AboutPage,
+  AuthWrapper,
+  CartPage,
+  CheckoutPage,
+  ErrorPage,
+  HomePage,
+  PrivateRute,
+  ProductsPage,
+  SingleProduct,
+} from './pages';
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <Router>
+      <Sidebar />
+      <Navbar />
+      <Switch>
+        <Route path='/' exact>
+          <HomePage />
+        </Route>
+        <Route path='/about' exact>
+          <AboutPage />
+        </Route>
+        <Route path='/cart' exact>
+          <CartPage />
+        </Route>
+        <Route path='/products' exact>
+          <ProductsPage />
+        </Route>
+        <Route path='/products/:id' children={<SingleProduct />} exact></Route>
+        <Route path='/checkout' exact>
+          <CheckoutPage />
+        </Route>
+        <Route path='*'>
+          <ErrorPage />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
+  );
 }
 
-export default App
+export default App;
