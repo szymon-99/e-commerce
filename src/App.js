23b1f388f78afee45b93275b810650b3c915e790
Szymon Prusak <szampons.szymon@gmail.com>
2021-05-05 20:47:46 +0200

    auth wrappper

--- src/App.js
+++ src/App.js
@@ -15,32 +15,38 @@ import {
 
 function App() {
   return (
-    <Router>
-      <Sidebar />
-      <Navbar />
-      <Switch>
-        <Route path='/' exact>
-          <HomePage />
-        </Route>
-        <Route path='/about' exact>
-          <AboutPage />
-        </Route>
-        <Route path='/cart' exact>
-          <CartPage />
-        </Route>
-        <Route path='/products' exact>
-          <ProductsPage />
-        </Route>
-        <Route path='/products/:id' children={<SingleProduct />} exact></Route>
-        <PrivateRute path='/checkout' exact>
-          <CheckoutPage />
-        </PrivateRute>
-        <Route path='*'>
-          <ErrorPage />
-        </Route>
-      </Switch>
-      <Footer />
-    </Router>
+    <AuthWrapper>
+      <Router>
+        <Sidebar />
+        <Navbar />
+        <Switch>
+          <Route path='/' exact>
+            <HomePage />
+          </Route>
+          <Route path='/about' exact>
+            <AboutPage />
+          </Route>
+          <Route path='/cart' exact>
+            <CartPage />
+          </Route>
+          <Route path='/products' exact>
+            <ProductsPage />
+          </Route>
+          <Route
+            path='/products/:id'
+            children={<SingleProduct />}
+            exact
+          ></Route>
+          <PrivateRute path='/checkout' exact>
+            <CheckoutPage />
+          </PrivateRute>
+          <Route path='*'>
+            <ErrorPage />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
+    </AuthWrapper>
   );
 }
 
--- src/context/user_context.js
+++ src/context/user_context.js
@@ -3,22 +3,12 @@ import { useAuth0 } from '@auth0/auth0-react';
 
 const UserContext = React.createContext();
 export const UserProvider = ({ children }) => {
-  const {
-    user,
-    isAuthenticated,
-    loginWithRedirect,
-    logout,
-    isLoading,
-  } = useAuth0();
+  const { user, loginWithRedirect, logout } = useAuth0();
   const [myUser, setMyUser] = useState(null);
 
   useEffect(() => {
-    if (isAuthenticated) {
-      setMyUser(user);
-    } else {
-      setMyUser(null);
-    }
-  }, [isAuthenticated]);
+    setMyUser(user);
+  }, [user]);
 
   return (
     <UserContext.Provider value={{ loginWithRedirect, logout, myUser }}>
