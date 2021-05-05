5cf9b9151cd6b5c6ca6eab243fc6cb1a3f87cc97
Szymon Prusak <szampons.szymon@gmail.com>
2021-05-05 12:57:26 +0200

    cart finished

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -39,6 +39,7 @@ export const CartProvider = ({ children }) => {
 
   useEffect(() => {
     localStorage.setItem('cart', JSON.stringify(state.cart));
+    dispatch({ type: COUNT_CART_TOTALS });
   }, [state.cart]);
   return (
     <CartContext.Provider
--- src/reducers/cart_reducer.js
+++ src/reducers/cart_reducer.js
@@ -68,6 +68,22 @@ const cart_reducer = (state, action) => {
 
       return { ...state, cart: tempCart };
     }
+    case COUNT_CART_TOTALS: {
+      const { total_amount, total_items } = state.cart.reduce(
+        (total, cartItem) => {
+          const { price, amount } = cartItem;
+          total.total_items += amount;
+          total.total_amount += price * amount;
+
+          return total;
+        },
+        {
+          total_items: 0,
+          total_amount: 0,
+        }
+      );
+      return { ...state, total_amount, total_items };
+    }
   }
 
   throw new Error(`No Matching "${action.type}" - action type`);
